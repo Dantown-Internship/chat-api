a09cc218a27e8029fb7f1b8950e264e915ffa09d
Presh <lukeprecious@gmail.com>
2022-10-28 14:54:19 +0100

    schema

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,14 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Events\Message;
+use App\Models\Message as ModelsMessage;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
     public function message(Request $request)
     {
-        // event(new Message($request->input('username'), $request->input('message')));
 
-        return [];
+        $request->validate([
+            'message' => ['required'],
+
+        ]);
+
+
+        // event(new Message($request->input('username'), $request->input('message')));
+        event(new Message( $request->message));
+
+        $newMessage = new ModelsMessage();
+
+        if($newMessage->user_id != 1){
+
+            $newMessage->user_id = 1;
+            $newMessage->message =  $request->message;
+            $newMessage->status = 'pending';
+
+        }
+
+
+
+        if($newMessage->user_id == 1){
+            $newMessage->message =  $request->message;
+            $newMessage->status = 'responded';
+
+        }
+
+        $newMessage->save();
+
+        return response()->json([
+            'success' => true,
+            'msg' => 'Message sent'
+        ]);
     }
 }
